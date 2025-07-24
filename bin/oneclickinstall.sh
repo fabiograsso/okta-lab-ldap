332dd08f030cc42609aece8e855c23829fad490b
Fabio Grasso <fabio.grasso@okta.com>
2025-07-24 23:51:41 +0200

    Various fixes

--- bin/oneclickinstall.sh
+++ bin/oneclickinstall.sh
@@ -15,11 +15,9 @@
 
 set -e
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-BASE_DIR==$(dirname "$SCRIPT_DIR")
+BASE_DIR=$(dirname "$SCRIPT_DIR")
 
-echo $SCRIPT_DIR
-echo $BASE_DIR
-pause
+mkdir /tmp/ldap-setup/
 
 # --- VERIFY OPERATING SYSTEM ---
 if [ -f /etc/os-release ]; then
@@ -33,7 +31,7 @@ else
     echo "ERROR: Cannot determine OS version. /etc/os-release not found."
     exit 1
 fi
-if ! ls "$BASE_DIR/packages/*.deb" 1>/dev/null 2>&1; then
+if ! ls $BASE_DIR/packages/*.deb 1>/dev/null 2>&1; then
     echo "ERROR: No Okta LDAP Agent installer (.deb file) found in the '$BASE_DIR/packages/' directory."
     echo "Please download the agent from your Okta Admin Console and place it in that folder before running this script."
     exit 1
@@ -50,7 +48,7 @@ else
     exit 1
 fi
 
-for var in "OKTA_ORG" "LDAP_DOMAIN" "LDAP_ADMIN_PASSWORD" "LDAP_CONFIG_PASSWORD"; do
+for var in "OKTA_ORG" "BASE_DN" "LDAP_ADMIN_PASSWORD" "LDAP_CONFIG_PASSWORD"; do
     if [ -z "${!var}" ]; then
         echo "ERROR: Required variable '${var}' was not loaded or is empty."
         echo "Please check your .env file."
@@ -61,9 +59,35 @@ done
 # --- SCRIPT START ---
 
 BASE_DN=$(echo "$LDAP_DOMAIN" | sed 's/\./,dc=/g' | sed 's/^/dc=/')
+LDAP_DOMAIN=$(echo "$BASE_DN" | sed -E 's/dc=([^,]+),?/\1./g' | sed 's/\.$//')
 ADMIN_DN="cn=admin,$BASE_DN"
 
 echo "--- Starting OpenLDAP and Utilities Installation on Ubuntu 24.04 ---"
+echo ""
+echo "Using the following settings: "
+echo "  - BASE_DN: $BASE_DN"
+echo "  - LDAP_DOMAIN: $LDAP_DOMAIN"
+echo "  - ADMIN_DN: $ADMIN_DN"
+echo "  - LDAP_ADMIN_PASSWORD: $LDAP_ADMIN_PASSWORD"
+echo "  - LDAP_CONFIG_PASSWORD: $LDAP_CONFIG_PASSWORD"
+echo ""
+
+# Ask for confirmation if not silent
+if [[ ! " $* " =~ " -s " && ! " $* " =~ " --silent " ]]; then
+  read -p "Continue? [Y/n] " CONFIRM
+  CONFIRM=${CONFIRM:-y}  # Default to 'y' if empty
+  case "$CONFIRM" in
+    [yY][eE][sS]|[yY]) 
+      echo "Continuing..."
+      ;;
+    *)
+      echo "Aborted."
+      exit 1
+      ;;
+  esac
+else
+  echo "Silent mode: skipping confirmation"
+fi
 
 # 1. System Preparation
 echo "--> Preparing system..."
@@ -90,16 +114,9 @@ echo "slapd slapd/backend select MDB" | sudo debconf-set-selections
 
 # 3. Install OpenLDAP
 echo "--> Installing slapd and ldap-utils..."
-sudo apt-get install -y slapd ldap-utils
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
 
-# 4. Load LDIF data
-echo "--> Loading data into LDAP..."
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ../src/ldifs/1.ou.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ../src/ldifs/2.users.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ../src/ldifs/3.groups.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ../src/ldifs/4.photos.ldif
-
-# 5. Configure cn=config user
+# 4. Configure cn=config user and root access
 echo "--> Set cn=config password..."
 sudo ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={0}config,cn=config
@@ -108,6 +125,32 @@ replace: olcRootPW
 olcRootPW: $(slappasswd -s "$LDAP_CONFIG_PASSWORD")
 EOF
 
+echo "--> Enabling root access into LDAP..."
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={0}config,cn=config
+changetype: modify
+replace: olcAccess
+olcAccess: {0}to * 
+  by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" manage
+  by dn.base="cn=config" manage
+  by * none
+EOF
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={1}mdb,cn=config
+changetype: modify
+replace: olcAccess
+olcAccess: {0}to * 
+  by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" manage
+  by dn.base="cn=admin,dc=galaxy,dc=universe" manage
+  by * none
+EOF
+
+# 5. Load LDIF data
+echo "--> Loading data into LDAP..."
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f $BASE_DIR/src/ldifs/1.ou.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f $BASE_DIR/src/ldifs/2.users.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f $BASE_DIR/src/ldifs/3.groups.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f $BASE_DIR/src/ldifs/4.photos.ldif
 
 # 6. Install and Configure ldap-ui
 echo "--> Installing and configuring ldap-ui..."
@@ -128,15 +171,15 @@ BindsTo=slapd.service
 
 [Service]
 WorkingDirectory=/opt/ldap-ui
-ExecStart=/opt/ldap-ui/.venv3/bin/ldap-ui
+ExecStart=/opt/ldap-ui/.venv3/bin/ldap-ui --host 0.0.0.0 --port 5000
 Type=simple
 User=ldap-ui
 Group=ldap-ui
 Restart=always
-Environment="BASE_DN=$BASE_DN"
-Environment="LDAP_URL=ldap://127.0.0.1"
-Environment="LOGIN_ATTR=cn"
-Environment="BIND_PATTERN=cn=%s,$BASE_DN"
+Environment=BASE_DN=$BASE_DN
+Environment=LDAP_URL=ldap://127.0.0.1
+Environment=LOGIN_ATTR=cn"
+Environment=BIND_PATTERN=cn=%s,$BASE_DN
 ProtectHome=true
 ProtectSystem=strict
 SystemCallFilter=@system-service
@@ -146,14 +189,15 @@ NoNewPrivileges=true
 WantedBy=multi-user.target
 EOF
 
-sudo mv /tmp/ldap-setup/ldap-ui.service /etc/systemd/system/
+sudo mv /tmp/ldap-setup/ldap-ui.service /etc/systemd/system/ldap-ui.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now ldap-ui.service
+#sudo systemctl start ldap-ui.service
 
-# 7. Okta LDAP Agent Setup
+# 7. Okta LDAP Agent Setupldap-ui.service
 echo "--> Preparing for Okta LDAP Agent..."
-cat > /opt/Okta/OktaLDAPAgent/conf/InstallOktaLDAPAgent.conf << EOF
-orgUrl=https://$OKTA_ORG
+sudo tee /opt/Okta/OktaLDAPAgent/conf/InstallOktaLDAPAgent.conf > /dev/null <<EOF
+orgUrl=https://$OKTA_ORG/
 ldapHost=localhost
 ldapAdminDN=$ADMIN_DN
 ldapPort=636
@@ -164,6 +208,7 @@ proxyEnabled=false
 sslPinningEnabled=true
 fipsMode=true
 EOF
+sudo chown OktaLDAPService:OktaLDAPService /opt/Okta/OktaLDAPAgent/conf/InstallOktaLDAPAgent.conf
 sudo dpkg -i $BASE_DIR/packages/OktaLDAPAgent-*.deb
 
 # 8. Cleanup
